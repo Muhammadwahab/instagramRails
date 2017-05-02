4fb2a31335890b6c70825ff38ef60825dd7b1df7
Abdul wahab <mwahab@live.com>
2017-05-02 10:51:49 +0500

    update complete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,6 +13,16 @@ class PostsController < ApplicationController
   	@post = Post.find(params[:id])
 	end  
 
+	def edit  
+		@post = Post.find(params[:id])
+end
+
+def update  
+	@post = Post.find(params[:id])
+  @post.update(post_params)
+  redirect_to(post_path(@post))
+end 
+
 	private
 	def post_params  
 	  params.require(:post).permit(:image, :caption)
